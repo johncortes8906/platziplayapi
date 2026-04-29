2936b70c46b1e90ada3bf70b564841d7c476227f
johncortes8906 <johncortes8906@gmail.com>
2026-04-29 12:10:38 -0500

    Use application properties

--- src/main/java/com/platzi/platziplayapi/HelloController.java
+++ src/main/java/com/platzi/platziplayapi/HelloController.java
@@ -1,5 +1,6 @@
 package com.platzi.platziplayapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,15 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private PlatziPlayAiService aiService;
+    private final String platform;
 
-    public HelloController(PlatziPlayAiService aiService) {
+    public HelloController(PlatziPlayAiService aiService,
+                           @Value("${spring.application.name}") String platform) {
         this.aiService = aiService;
+        this.platform = platform;
     }
 
     @GetMapping("/")
     public String hello() {
 
-        return this.aiService.generateGretting();
+        return this.aiService.generateGretting(platform);
     }
 
 }
--- src/main/java/com/platzi/platziplayapi/PlatziPlayAiService.java
+++ src/main/java/com/platzi/platziplayapi/PlatziPlayAiService.java
@@ -1,13 +1,14 @@
 package com.platzi.platziplayapi;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface PlatziPlayAiService {
     
-    @UserMessage("Create a friendly greeting to welcome user to Platzi Play platform where the user" +
+    @UserMessage("Create a friendly greeting to welcome user to {{platform}} platform where the user" +
             " can watch multiple movies and documentaries. Use no more than 90 characters and apply the Platzi style." +
             "Use only English in your output.")
-    String generateGretting();
+    String generateGretting(@V("platform") String platform);
 }
